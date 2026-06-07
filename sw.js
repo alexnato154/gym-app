bd3b2c9b83053738e4edf3bcc5314ed415abad6d
alexnato154 <alexnato154@gmail.com>
2026-06-07 14:55:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const CACHE = 'gymtracker-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'];
+const ASSETS = ['/gym-app/', '/gym-app/index.html', '/gym-app/manifest.json',
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
